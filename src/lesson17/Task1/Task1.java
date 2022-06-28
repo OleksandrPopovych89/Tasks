69d2948738a11947681a125b5bee6f17f19fafbd
Oleksandr <oleksandrpopovych89@gmail.com>
2022-06-28 18:04:27 +0300

    Completed task 1.

--- src/lesson17/Task1/Task1.java
+++ src/lesson17/Task1/Task1.java
@@ -15,8 +15,35 @@ Lesson 17, task 1:
 */
 package lesson17.Task1;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list = new ArrayList<>();
+
+        String temp;
+
+        while (true) {
+            temp = reader.readLine();
+            if (temp.isEmpty() == false) {
+                list.add(temp);
+            } else {
+                break;
+            }
+        }
+        for (String s : list) {
+            if (s.length() % 2 == 0) {
+                System.out.println(s + " " + s);
+            } else {
+                System.out.println(s + " " + s + " " + s);
+            }
+        }
     }
 }
